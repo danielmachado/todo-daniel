ff0a2770ffbee60daabac3d6ccf08171e91ba087
Daniel Machado Fernández <daniel.machado.fernandez@gmail.com>
2012-02-22 19:52:59 +0100

    Añadimos rails al proyecto

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TodoDaniel::Application.config.secret_token = '339b8390cdd01898fededb1c3e2154843958e2b4e22333d466c218f1ab15e13681be1a2dc270cb1fc5e058de0fed8906ae2ef9d4813d4e7c0d2ba9a0bc8cad73'
