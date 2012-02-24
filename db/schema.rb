750ed19f50511aea7705ee6e0d0ef2fa43a29bb8
Daniel Machado Fernández <daniel.machado.fernandez@gmail.com>
2012-02-24 17:31:58 +0100

    Migración de BBDD con la relación de Project y Task

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120222192902) do
+ActiveRecord::Schema.define(:version => 20120224160720) do
 
   create_table "projects", :force => true do |t|
     t.string   "title"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(:version => 20120222192902) do
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
     t.integer  "user_id"
+    t.integer  "project_id"
   end
 
   create_table "users", :force => true do |t|
