a3d80abfec5f2318038d20c1f1b47a34634ed361
Daniel Machado Fernández <daniel.machado.fernandez@gmail.com>
2012-02-24 20:25:10 +0100

    Scopes añadidos al modelo Task

--- app/models/task.rb
+++ app/models/task.rb
@@ -5,4 +5,16 @@ class Task < ActiveRecord::Base
 
 	belongs_to :user
 	belongs_to :project
+
+	scope :nearly_completed, :conditions => ["percentage >= ?", 90]
+	scope :not_assigned, :conditions => ["user_id IS NULL"]
+	scope :assigned, :conditions => ["user_id NOT NULL"]
+	scope :almost_due, lambda{ 
+		{:conditions => ["due_at < ?", 7.days.from_now]}
+	}
+
+	scope :for_user, lambda{ |user|
+		{:conditions => ["user_id = ?",user.id]}
+	}
+
 end
